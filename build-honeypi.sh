267026cf75bbd4a78b8e16118ceb895cc6b430cf
Javan Rasokat <mail@javan.de>
2021-01-29 20:57:10 +0100

    update to latest Raspberry OS

--- build-honeypi.sh
+++ build-honeypi.sh
@@ -4,19 +4,20 @@
 git clone https://github.com/RPi-Distro/pi-gen
 cd pi-gen
 git fetch && git fetch --tags
-git checkout 2020-02-05-raspbian-buster
+#git checkout 2020-02-05-raspbian-buster # All images up to HoneyPi v1.0.8-beta10
+git checkout 2020-12-02-raspbian-buster
 cd ..
 
-touch pi-gen/stage5/SKIP_IMAGES
-touch pi-gen/stage5/SKIP
-
-touch pi-gen/stage4/SKIP_IMAGES
-touch pi-gen/stage4/SKIP
+# not used because /config already defines STAGE_LIST
+#touch pi-gen/stage3/SKIP pi-gen/stage4/SKIP pi-gen/stage5/SKIP
+#touch pi-gen/stage3/SKIP_IMAGES pi-gen/stage4/SKIP_IMAGES pi-gen/stage5/SKIP_IMAGES
 
 # copy config
+[ -e pi-gen/config ] && rm -R pi-gen/config
 cp config pi-gen/config
 
 # copy custom stage
+[ -e pi-gen/stage-honeypi ] && rm -R pi-gen/stage-honeypi
 cp -R stage-honeypi pi-gen/stage-honeypi
 
 case "$OSTYPE" in
@@ -28,5 +29,6 @@ esac
 
 echo "Running build..."
 cd pi-gen
-./build-docker.sh
+CONTINUE=1 PRESERVE_CONTAINER=1 ./build-docker.sh
+#./build-docker.sh
 #./build.sh
