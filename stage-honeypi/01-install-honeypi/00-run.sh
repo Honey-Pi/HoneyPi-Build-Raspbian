32c3ed896b29e1bba4344a1a698b81baa993a4b2
Javan Rasokat <mail@javan.de>
2023-12-27 18:49:03 +0100

    another try

--- stage-honeypi/01-install-honeypi/00-run.sh
+++ stage-honeypi/01-install-honeypi/00-run.sh
@@ -115,7 +115,10 @@ echo '>>> Install deprecated DHT library for measurement python scripts'
 # deprecated, but still used for Pi Zero WH because of known issues such as https://github.com/adafruit/Adafruit_CircuitPython_DHT/issues/73 - no longer working on bullseye
 python3 -m pip install --upgrade setuptools wheel # see: https://stackoverflow.com/a/72934737/6696623
 # Fake Raspberry Pi version because --install-option="--force-pi" does not work with pip v22.3
-echo -e "\nHardware   : BCM2709" >> /proc/cpuinfo # see: https://github.com/adafruit/Adafruit_Python_DHT/blob/8f5e2c4d6ebba8836f6d31ec9a0c171948e3237d/Adafruit_DHT/platform_detect.py#L36
+echo -e "\nHardware   : BCM2709" >> /etc/cpuinfo # see: https://github.com/adafruit/Adafruit_Python_DHT/blob/8f5e2c4d6ebba8836f6d31ec9a0c171948e3237d/Adafruit_DHT/platform_detect.py#L36
+if [ -e /etc/cpuinfo] ; then
+  mount—bind /etc/cpuinfo /proc/cpuinfo
+fi
 pip3 install Adafruit_DHT
 pip3 install Adafruit_Python_DHT
 
